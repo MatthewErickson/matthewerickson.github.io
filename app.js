f7a988aebcb66def8a9fe302e98ab5124516828f
Matthew Erickson <matterickson915@gmail.com>
2020-08-09 14:37:13 -0700

    Add new hunt

--- app.js
+++ app.js
@@ -5,31 +5,7 @@ function encrypt(message, key) {
 function decrypt(encrypted, key) {
     return CryptoJS.AES.decrypt(encrypted, key).toString(CryptoJS.enc.Utf8);
 }
-
-function submitAnswer() {
-    $("#data-output")[0].innerHTML = "";
-    $("#data-output")[0].style = "color: white";
-    var answer = $("#data1")[0].value;
-    for (var i in encrypted_data) {
-        try {
-            var decrypted = decrypt(encrypted_data[i], answer);
-            if (decrypted && decrypted != "rb") {
-                $("#data-output")[0].innerHTML = decrypted;
-                return;
-            }
-        } catch (e){
-        }
-    }
-    error();
-}
-
-function error() {
-    $("#data-output")[0].innerHTML = "Decryption failed.";
-    $("#data-output")[0].style = "color: red";
-}
-
-// Nice try :)
-var encrypted_data = [
+var encrypted_data_redmond = [
     "U2FsdGVkX19a1yR+RmsgLT1RjDwjOsOB/z11BgrROAQqoFJsO24074JFM4q7EEEFMzk5v0uMVoHboAcnwjRQvQJIvCxM+nre15VC6KR4lfbz3gjtPHk+rRWsVQAY4QAq",
     "U2FsdGVkX19Opwyf4unwiJGjiute3D6ZEnK6v4CKhwxK2nDXD6HI3bspauN7Uv/VQSSyLOlcSBf7OYo1g+KmEVfCpdf61U+CBBpW2KcM7TTFbp9+II1qGpsEdCHSkWF9PyspDiPu7SqoHPnMWy602g==",
     "U2FsdGVkX18u+mNP00is6WgWCH/3CgJJVLjU/+hpbXxrbvzJveYFd/R1x7YYm/ZFtAfz2QEaw0dEvQ7R4zhdXTI2XJ9vxlUpyrt44Pl3c35926cd82ng+Z+du1KlZwauaIJumeIa7DD+6LEQMjcQkYvXaUZv2QUpD4tIUiZ9u8Y=",
@@ -38,3 +14,44 @@ var encrypted_data = [
     "U2FsdGVkX1/4zNyqHjv7uh7U6HCkFbBQd+t43YNiHedkGm3WuUZ91xKF72+/Rxs4sOHJBG83AceDZ3hu8jmexew1lv0PwtOGvHHavycWGX4=",
     "U2FsdGVkX19uwidgowph7wn/c0PSABfqWHptkA2U89Il4KNJ4sF9pAAM3wou5CX2UAMzyLn9lTnoJEIIDV00AbD81m8021CabMOMCGw3itK74dA0mjyWtv7Pk1OOdOnsw9wsbx1oKZfkMJruRPwyzHFYAP+eUBCfszDAFp2YN58="
 ];
+
+function submitRedmondAnswer() {
+    $("#data-output-redmond")[0].innerHTML = "";
+    $("#data-output-redmond")[0].style = "color: white";
+    var answer = $("#data-redmond")[0].value;
+    for (var i in encrypted_data_redmond) {
+        try {
+            var decrypted = decrypt(encrypted_data_redmond[i], answer);
+            if (decrypted && decrypted != "rb") {
+                $("#data-output-redmond")[0].innerHTML = decrypted;
+                return;
+            }
+        } catch (e){
+        }
+    }
+    $("#data-output-redmond")[0].innerHTML = "Decryption failed.";
+    $("#data-output-redmond")[0].style = "color: red";
+}
+
+var encrypted_data_public = [
+    "U2FsdGVkX18U/llSeXdIE4P0kn4XxalvFJWptbCnt/AbgDZUwFU6ISId9nd9aSSm",
+    "U2FsdGVkX18FDIJLhRZFGq4njDagWN8euS8VNAtxm2eUm+5mznP7jYND3bFLpq4WGAnQmggsJYakRoSBZcSYJSejJiaBLuiEYa6pgrHKpcEkQ8JpPX6XanNwnXNAYwMw+TzsYQ5pfHmPitO3IKGFJw=="
+];
+
+function submitPublicAnswer() {
+    $("#data-output-public")[0].innerHTML = "";
+    $("#data-output-public")[0].style = "color: white";
+    var answer = $("#data-public")[0].value;
+    for (var i in encrypted_data_public) {
+        try {
+            var decrypted = decrypt(encrypted_data_public[i], answer);
+            if (decrypted && decrypted != "rb") {
+                $("#data-output-public")[0].innerHTML = decrypted;
+                return;
+            }
+        } catch (e){
+        }
+    }
+    $("#data-output-public")[0].innerHTML = "Decryption failed.";
+    $("#data-output-public")[0].style = "color: red";
+}
